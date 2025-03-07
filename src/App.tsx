c7e03efffd43cf860ecdada0ee1a5ae548eaced9
kevinroberts1117 <kevinroberts1117@outlook.com>
2025-03-07 05:42:04 -0500

    display logo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
-import PhantomWallet from "./PhantomWallet";
+// import PhantomWallet from "./PhantomWallet";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/wallet" element={<PhantomWallet children={null} />} />
+        {/* <Route path="/wallet" element={<PhantomWallet children={null} />} /> */}
       </Routes>
     </BrowserRouter>
   );
